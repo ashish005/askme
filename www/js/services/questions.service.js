ab768f7718a2c966019f643730e036524fad75cd
atinepro <atinder@wizni.com>
2016-03-10 01:34:01 +0530

    Ask me form

--- www/js/services/questions.service.js
+++ www/js/services/questions.service.js
@@ -18,39 +18,8 @@
         url: '/record',
         method: 'get'
       }).then(function (response) {
-        return response.data;
+        return response.data.data;
       });
-
-      var defer = $q.defer();
-      defer.resolve([
-        {
-          "_id": "56e000469403e3382b610b4b",
-          "userId": "ashi",
-          "userName": "Ashish Chaturvedi",
-          "presidentId": "hari",
-          "presidentName": "Harish Tejwani",
-          "form": {"type": "issue", "topic": "what", "subject": "sub", "comment": "comment"},
-          "answer": "I am answering for the question",
-          "isAnswered": true,
-          "createdDate": "2016-03-09T10:51:43.951Z",
-          "updatedDate": "2016-03-09T10:51:43.951Z",
-          "likeCount": 20
-        },
-        {
-          "_id": "56e000469403e3382b610b4b",
-          "userId": "ashi",
-          "userName": "Ashish Chaturvedi",
-          "presidentId": "hari",
-          "presidentName": "Harish Tejwani",
-          "form": {"type": "issue", "topic": "what", "subject": "sub", "comment": "comment"},
-          "answer": "I am answering for the question",
-          "isAnswered": true,
-          "createdDate": "2016-03-09T10:51:43.951Z",
-          "updatedDate": "2016-03-09T10:51:43.951Z",
-          "likeCount": 20
-        }
-      ]);
-      return defer.promise;
     }
 
     function getAnsweredQuestions() {
@@ -58,73 +27,17 @@
         url: '/record',
         method: 'get'
       }).then(function (response) {
-        return response.data;
+        return response.data.data;
       });
-
-
-      var defer = $q.defer();
-      defer.resolve([
-        {
-          "_id": "56e000469403e3382b610b4b",
-          "userId": "ashi",
-          "userName": "Ashish Chaturvedi",
-          "presidentId": "hari",
-          "presidentName": "Harish Tejwani",
-          "form": {"type": "issue", "topic": "what", "subject": "sub", "comment": "comment"},
-          "answer": "I am answering for the question",
-          "isAnswered": true,
-          "createdDate": "2016-03-09T10:51:43.951Z",
-          "updatedDate": "2016-03-09T10:51:43.951Z",
-          "likeCount": 20
-        },
-        {
-          "_id": "56e000469403e3382b610b4b",
-          "userId": "ashi",
-          "userName": "Ashish Chaturvedi",
-          "presidentId": "hari",
-          "presidentName": "Harish Tejwani",
-          "form": {"type": "issue", "topic": "what", "subject": "sub", "comment": "comment"},
-          "answer": "I am answering for the question",
-          "isAnswered": true,
-          "createdDate": "2016-03-09T10:51:43.951Z",
-          "updatedDate": "2016-03-09T10:51:43.951Z",
-          "likeCount": 20
-        }
-      ]);
-      return defer.promise;
     }
 
     function getFavouriteQuestions() {
-      var defer = $q.defer();
-      defer.resolve([
-        {
-          "_id": "56e000469403e3382b610b4b",
-          "userId": "ashi",
-          "userName": "Ashish Chaturvedi",
-          "presidentId": "hari",
-          "presidentName": "Harish Tejwani",
-          "form": {"type": "issue", "topic": "what", "subject": "sub", "comment": "comment"},
-          "answer": "I am answering for the question",
-          "isAnswered": true,
-          "createdDate": "2016-03-09T10:51:43.951Z",
-          "updatedDate": "2016-03-09T10:51:43.951Z",
-          "likeCount": 20
-        },
-        {
-          "_id": "56e000469403e3382b610b4b",
-          "userId": "ashi",
-          "userName": "Ashish Chaturvedi",
-          "presidentId": "hari",
-          "presidentName": "Harish Tejwani",
-          "form": {"type": "issue", "topic": "what", "subject": "sub", "comment": "comment"},
-          "answer": "I am answering for the question",
-          "isAnswered": true,
-          "createdDate": "2016-03-09T10:51:43.951Z",
-          "updatedDate": "2016-03-09T10:51:43.951Z",
-          "likeCount": 20
-        }
-      ]);
-      return defer.promise;
+      return $http({
+        url: '/record',
+        method: 'get'
+      }).then(function (response) {
+        return response.data.data;
+      });
     }
   }
 })();
